c32d0aa695215cdf3c5d548ecef7e14e842b287c
ramsteel <ramajg8800@gmail.com>
2024-05-04 05:15:53 +0800

    feat: add logger config

---
+++ api/middlewares/logger.go
@@ -0,0 +1,9 @@
+package middlewares
+
+import "github.com/labstack/echo/v4/middleware"
+
+func GetLoggerConfig() *middleware.LoggerConfig {
+	return &middleware.LoggerConfig{
+		Format: "method=${method}, url=${host}${uri}, status=${status}, latency=${latency_human}\n",
+	}
+}
