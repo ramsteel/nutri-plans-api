b231704d8015745374df3f5e7eb6dcc807d79912
ramsteel <ramajg8800@gmail.com>
2024-05-06 20:20:21 +0800

    feat: add manage profile repository layer

--- repositories/user.go
+++ repositories/user.go
@@ -4,11 +4,15 @@ import (
 	"context"
 	"nutri-plans-api/entities"
 
+	"github.com/google/uuid"
 	"gorm.io/gorm"
+	"gorm.io/gorm/clause"
 )
 
 type UserRepository interface {
 	CreateUser(ctx context.Context, user *entities.User) error
+	GetUserByID(ctx context.Context, id uuid.UUID) (*entities.User, error)
+	UpdateUser(ctx context.Context, user *entities.User) error
 }
 
 type userRepository struct {
@@ -27,4 +31,28 @@ func (u *userRepository) CreateUser(ctx context.Context, user *entities.User) er
 	}
 
 	return u.db.Create(user).Error
+}
+
+func (u *userRepository) GetUserByID(ctx context.Context, id uuid.UUID) (*entities.User, error) {
+	if err := ctx.Err(); err != nil {
+		return nil, err
+	}
+
+	user := &entities.User{AuthID: id}
+	err := u.db.Preload("Auth.RoleType").Preload(clause.Associations).First(user).Error
+	if err != nil {
+		return nil, err
+	}
+
+	return user, nil
+}
+
+func (u *userRepository) UpdateUser(ctx context.Context, user *entities.User) error {
+	if err := ctx.Err(); err != nil {
+		return err
+	}
+
+	return u.db.Omit(
+		"Auth.Password",
+	).Session(&gorm.Session{FullSaveAssociations: true}).Updates(user).Error
 }
