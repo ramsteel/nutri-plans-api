4d5818459ef561e3e67b7b8c46a74492ac93c6e1
ramsteel <ramajg8800@gmail.com>
2024-05-08 00:39:39 +0800

    feat: add particular search response

--- dto/response.go
+++ dto/response.go
@@ -5,3 +5,15 @@ type BaseResponse struct {
 	Message string `json:"message"`
 	Data    any    `json:"data,omitempty"`
 }
+
+type SearchResponse struct {
+	BaseResponse
+	Metadata *MetadataResponse `json:"metadata,omitempty"`
+}
+
+type MetadataResponse struct {
+	TotalData   int  `json:"total_data"`
+	TotalCount  int  `json:"total_count"`
+	NextOffset  int  `json:"next_offset"`
+	HasLoadMore bool `json:"has_load_more"`
+}
