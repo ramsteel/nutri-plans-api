fa855943e7365e4b7755e407795fac1280c65996
ramsteel <ramajg8800@gmail.com>
2024-05-09 11:32:00 +0800

    fix: change data type to handle high precision floating number

--- entities/meal_item.go
+++ entities/meal_item.go
@@ -8,21 +8,21 @@ import (
 )
 
 type MealItem struct {
-	ID           uint64         `json:"-" gorm:"primaryKey;autoIncrement"`
+	ID           uint64         `json:"id" gorm:"primaryKey;autoIncrement"`
 	MealID       uuid.UUID      `json:"-" gorm:"type:uuid"`
 	Meal         Meal           `json:"-"`
 	MealTypeID   uint           `json:"-" gorm:"type:uint"`
 	MealType     MealType       `json:"meal_type"`
 	ItemName     string         `json:"item_name" gorm:"type:varchar(255)"`
-	Qty          float32        `json:"qty" gorm:"type:float4"`
+	Qty          float32        `json:"qty" gorm:"type:numeric(10, 2) not null"`
 	Unit         string         `json:"unit" gorm:"type:varchar(255)"`
-	Weight       float32        `json:"weight" gorm:"type:float4"`
-	Calories     float32        `json:"calories" gorm:"type:float4"`
-	Carbohydrate float32        `json:"carbohydrate" gorm:"type:float4"`
-	Protein      float32        `json:"protein" gorm:"type:float4"`
-	Fat          float32        `json:"fat" gorm:"type:float4"`
-	Cholesterol  float32        `json:"cholesterol" gorm:"type:float4"`
-	Sugars       float32        `json:"sugars" gorm:"type:float4"`
+	Weight       float32        `json:"weight" gorm:"type:numeric(10, 2) not null"`
+	Calories     float32        `json:"calories" gorm:"type:numeric(10, 2) not null"`
+	Carbohydrate float32        `json:"carbohydrate" gorm:"type:numeric(10, 2) not null"`
+	Protein      float32        `json:"protein" gorm:"type:numeric(10, 2) not null"`
+	Fat          float32        `json:"fat" gorm:"type:numeric(10, 2) not null"`
+	Cholesterol  float32        `json:"cholesterol" gorm:"type:numeric(10, 2) not null"`
+	Sugars       float32        `json:"sugars" gorm:"type:numeric(10, 2) not null"`
 	CreatedAt    time.Time      `json:"created_at"`
 	UpdatedAt    time.Time      `json:"updated_at"`
 	DeletedAt    gorm.DeletedAt `json:"-" gorm:"index"`
