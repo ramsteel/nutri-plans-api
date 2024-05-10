bf319e50a058ae94bd0369816892258c347b6e83
ramsteel <ramajg8800@gmail.com>
2024-05-11 00:19:31 +0800

    feat: add cron job for fetch recommendation and method to get user recommendation

---
+++ usecases/recommendation.go
@@ -0,0 +1,90 @@
+package usecases
+
+import (
+	"context"
+	"nutri-plans-api/dto"
+	exnutri "nutri-plans-api/externals/nutrition"
+	exoai "nutri-plans-api/externals/openai"
+	"nutri-plans-api/repositories"
+	"nutri-plans-api/utils/prompt"
+	recutil "nutri-plans-api/utils/recommendation"
+
+	"github.com/google/uuid"
+	"github.com/labstack/echo/v4"
+	"github.com/robfig/cron"
+)
+
+type RecommendationUsecase interface {
+	GetRecommendation(c echo.Context, uid uuid.UUID) (*[]exnutri.ItemNutrition, error)
+}
+
+type recommendationUsecase struct {
+	recommendationRepo repositories.RecommendationRepository
+	userPrefRepo       repositories.UserPreferenceRepository
+
+	nutritionExternal exnutri.NutritionClient
+	oaiExternal       exoai.OpenAIClient
+}
+
+func NewRecommendationUsecase(
+	recommendationRepo repositories.RecommendationRepository,
+	userPrefRepo repositories.UserPreferenceRepository,
+	nutritionExternal exnutri.NutritionClient,
+	recExternal exoai.OpenAIClient,
+) *recommendationUsecase {
+	return &recommendationUsecase{
+		recommendationRepo: recommendationRepo,
+		userPrefRepo:       userPrefRepo,
+		nutritionExternal:  nutritionExternal,
+		oaiExternal:        recExternal,
+	}
+}
+
+func (r *recommendationUsecase) StartRecommendationCron() {
+	c := cron.New()
+	c.AddFunc("@weekly", r.fetchOpenAIRecommendation)
+
+	go func() {
+		c.Start()
+		defer c.Stop()
+
+		select {}
+	}()
+}
+
+func (r *recommendationUsecase) GetRecommendation(
+	c echo.Context,
+	uid uuid.UUID,
+) (*[]exnutri.ItemNutrition, error) {
+	ctx, cancel := context.WithCancel(c.Request().Context())
+	defer cancel()
+
+	recommendations, err := r.recommendationRepo.GetRecommendations(ctx, uid)
+	if err != nil {
+		return nil, err
+	}
+
+	query := recutil.ToString(recommendations)
+	req := &dto.ItemNutritionRequest{
+		Query: query[0],
+	}
+	itemNutritions, err := r.nutritionExternal.GetMultipleItemNutritions(ctx, req)
+	if err != nil {
+		return nil, err
+	}
+
+	return itemNutritions, nil
+}
+
+func (r *recommendationUsecase) fetchOpenAIRecommendation() {
+	ctx := context.Background()
+	preferences, _ := r.userPrefRepo.GetAllUserPreferences(ctx)
+	for _, pref := range *preferences {
+		prompt := prompt.GetRecommendationPrompt(&pref)
+		res, err := r.oaiExternal.GetRecommendation(prompt, recutil.ToString(pref.Recommendations))
+		if err == nil {
+			recommendations := recutil.ToStruct(res, pref.UserID)
+			r.recommendationRepo.CreateRecommendations(ctx, recommendations)
+		}
+	}
+}
